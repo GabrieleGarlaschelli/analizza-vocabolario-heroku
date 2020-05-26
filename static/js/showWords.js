43f0c8d8ef5f29062ef7055f9efee302fce624fb
NicolaiOsalchuk <238998@studenti.unimore.it>
2020-05-26 11:52:17 +0200

    aggiornamento static

--- static/js/showWords.js
+++ static/js/showWords.js
@@ -2,6 +2,10 @@ function showWords() {
     var myTable = document.getElementById("Table");
     var checkBox = document.getElementById("blacklist");
 
+    for(var j = 0; j < blacklist_list.length; j++){
+        console.log(blacklist_list[j]);
+    }
+
 
     var dropdown = document.getElementById("wordselect");
     var dropdown_value = dropdown.options[dropdown.selectedIndex].value;
@@ -16,6 +20,7 @@ function showWords() {
             }
         } else if (dropdown_value == '2') {
             for(var i = 0; i < myTable.rows.length; i++) {
+                myTable.rows[i].style.display = "";
                 if (myTable.rows[i].cells[0].innerHTML.length < 3) {
                     myTable.rows[i].style.display = "none";
                 }
@@ -54,11 +59,14 @@ function showWords() {
             }
         }else {
             for(var i = 0; i < myTable.rows.length; i++) {
+                myTable.rows[i].style.display = "";
+                console.log(myTable.rows[i].cells[0].innerHTML);
                 for(var j = 0; j < blacklist_list.length; j++){
                     if(myTable.rows[i].cells[0].innerHTML == blacklist_list[j] ){
                         myTable.rows[i].style.display = "none";
-                    }else{
-                        myTable.rows[i].style.display = "";
+                    // }else{
+                    //     console.log(blacklist_list[j]+" == "+myTable.rows[i].cells[0].innerHTML);
+                    //     myTable.rows[i].style.display = "";
                     }
                 }
             }
